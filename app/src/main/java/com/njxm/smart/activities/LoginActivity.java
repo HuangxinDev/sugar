62849fe71534dc7d873b071d3fa9f09416b6a642
sugar <huang_xin88@163.com>
2019-11-30 22:59:11 +0800

    登录页面布局改造

--- app/src/main/java/com/njxm/smart/activities/LoginActivity.java
+++ app/src/main/java/com/njxm/smart/activities/LoginActivity.java
@@ -3,10 +3,12 @@ package com.njxm.smart.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.ns.demo.R;
 
@@ -27,12 +29,35 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
     // 快捷登录和密码登录下方的下划线
     private View mQuickLoginDivider, mPasswordLoginDivider;
 
+    // 账号验证布局
+    private ConstraintLayout mUserAccountLayout;
+
+    // 密码验证布局
+    private ConstraintLayout mUserPwdLayout;
+
+    // 图形验证布局
+    private ConstraintLayout mUserPicLayout;
+
+    // 验证码登录
+    private ConstraintLayout mUserCodeLayout;
+
     // 用户名编辑框
-    private EditText mUserAccountEditText;
+    private AppCompatEditText mUserAccountEditText;
 
     // 密码编辑框
-    private EditText mUserPasswordEditText;
+    private AppCompatEditText mUserPasswordEditText;
 
+    // 图形验证码编辑框
+    private AppCompatEditText mUserPicEditText;
+
+    // 验证码编辑框
+    private AppCompatEditText mUserVerifyCodeEditText;
+
+    // 获取验证码按钮
+    private AppCompatTextView mGetVerifyCodeBtn;
+
+    // 密码显示/隐藏按钮
+    private AppCompatTextView mShowPwdBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,8 +74,12 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
         mQuickLoginDivider = findViewById(R.id.quick_login_divider);
         mPasswordLoginDivider = findViewById(R.id.password_login_divider);
 
-        mUserPasswordEditText = findViewById(R.id.login_user_password_et);
+        mUserAccountLayout = findViewById(R.id.verify_account);
+        mUserPicLayout = findViewById(R.id.verify_picture);
+        mUserPwdLayout = findViewById(R.id.verify_pwd);
+        mUserCodeLayout = findViewById(R.id.verify_code);
 
+        switchLoginWay(true);
     }
 
     @Override
@@ -59,15 +88,36 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intent);
         } else if (v == mQuickLoginBtn) {
-            mQuickLoginBtn.setEnabled(false);
-            mPasswordLoginBtn.setEnabled(true);
-            mQuickLoginDivider.setVisibility(mQuickLoginBtn.isEnabled() ? View.INVISIBLE : View.VISIBLE);
-            mPasswordLoginDivider.setVisibility(mPasswordLoginBtn.isEnabled() ? View.INVISIBLE : View.VISIBLE);
+            switchLoginWay(false);
         } else if (v == mPasswordLoginBtn) {
-            mQuickLoginBtn.setEnabled(true);
+            switchLoginWay(true);
+        }
+    }
+
+    /**
+     * 切换登录方式
+     *
+     * @param pwdLogin 密码登录
+     */
+    private void switchLoginWay(boolean pwdLogin) {
+        if (pwdLogin) {
             mPasswordLoginBtn.setEnabled(false);
-            mQuickLoginDivider.setVisibility(mQuickLoginBtn.isEnabled() ? View.INVISIBLE : View.VISIBLE);
-            mPasswordLoginDivider.setVisibility(mPasswordLoginBtn.isEnabled() ? View.INVISIBLE : View.VISIBLE);
+            mQuickLoginBtn.setEnabled(true);
+            mPasswordLoginDivider.setVisibility(View.VISIBLE);
+            mQuickLoginDivider.setVisibility(View.INVISIBLE);
+
+            mUserPwdLayout.setVisibility(View.VISIBLE);
+            mUserPicLayout.setVisibility(View.GONE);
+            mUserCodeLayout.setVisibility(View.GONE);
+        } else {
+            mPasswordLoginBtn.setEnabled(true);
+            mQuickLoginBtn.setEnabled(false);
+            mQuickLoginDivider.setVisibility(View.VISIBLE);
+            mPasswordLoginDivider.setVisibility(View.INVISIBLE);
+
+            mUserPwdLayout.setVisibility(View.GONE);
+            mUserPicLayout.setVisibility(View.VISIBLE);
+            mUserCodeLayout.setVisibility(View.VISIBLE);
         }
     }
 }
