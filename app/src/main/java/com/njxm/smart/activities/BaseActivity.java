000adfa2a01936da189ae2370515f68122523d0a
sugar <huang_xin88@163.com>
2019-11-30 15:56:43 +0800

    刘海屏适配处理

--- app/src/main/java/com/njxm/smart/activities/BaseActivity.java
+++ app/src/main/java/com/njxm/smart/activities/BaseActivity.java
@@ -1,11 +1,15 @@
 package com.njxm.smart.activities;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.njxm.smart.utils.LogTool;
+import com.ns.demo.R;
 
 /**
  * 基类，提供共用方法和回调
@@ -24,6 +28,26 @@ public abstract class BaseActivity extends AppCompatActivity {
         LogTool.printI("[%s] %s", TAG, ">>> onCreate <<<");
 //        StatusBarUtil.setStatusBarColor(this, R.color.text_color_gray);
 
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        LinearLayout ll = findViewById(R.id.ll_root);
+        if (ll != null) {
+            ll.setPadding(0, getStatusBarHeight(this), 0, 0 );
+        }
+    }
+
+    /**
+     * 获取状态栏高度
+     *
+     * @param context
+     * @return
+     */
+    public int getStatusBarHeight(Context context) {
+        int statusBarHeight = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            statusBarHeight = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return statusBarHeight;
     }
 
 
