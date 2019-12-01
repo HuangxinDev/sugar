2d4fa592d277cadaf5168ad2acac92ee5befcfed
sugar <huang_xin88@163.com>
2019-12-01 23:42:42 +0800

    基类补充

--- app/src/main/java/com/njxm/smart/activities/BaseActivity.java
+++ app/src/main/java/com/njxm/smart/activities/BaseActivity.java
@@ -3,7 +3,9 @@ package com.njxm.smart.activities;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +13,16 @@ import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.appcompat.widget.AppCompatTextView;
 
 import com.njxm.smart.utils.LogTool;
+import com.njxm.smart.view.callbacks.OnActionBarChange;
 import com.ns.demo.R;
+
+import java.util.Locale;
 
 /**
  * 基类，提供共用方法和回调
  */
-public abstract class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity implements OnActionBarChange,
+        OnClickListener {
 
     protected final String TAG;
 
@@ -47,6 +53,13 @@ public abstract class BaseActivity extends AppCompatActivity {
         mActionBarBackBtn = findViewById(R.id.action_bar_left);
         mActionBarRightBtn = findViewById(R.id.action_bar_right);
         mActionBarTitle = findViewById(R.id.action_bar_title);
+        if (mActionBarBackBtn != null) {
+            mActionBarBackBtn.setOnClickListener(this);
+        }
+
+        if (mActionBarRightBtn != null) {
+            mActionBarRightBtn.setOnClickListener(this);
+        }
     }
 
     /**
@@ -74,5 +87,87 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     protected int setStatusBarColor() {
         return R.color.text_color_white;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mActionBarBackBtn) {
+            onClickLeftBtn();
+        } else if (v == mActionBarRightBtn) {
+            onClickRightBtn();
+        }
+    }
+
+    @Override
+    public void onClickLeftBtn() {
+
+    }
+
+    @Override
+    public void onClickRightBtn() {
+
+    }
+
+    @Override
+    public void setActionBarTitle(String title) {
+        if (mActionBarTitle != null) {
+            mActionBarTitle.setText(title);
+        }
+    }
+
+    @Override
+    public void showView(View view, boolean show) {
+        if (view != null) {
+            view.setVisibility(show ? View.VISIBLE : View.GONE);
+        }
+    }
+
+    @Override
+    public void setImageResource(AppCompatImageButton view, int resourceId) {
+        if (view != null) {
+            view.setImageResource(resourceId);
+        }
+    }
+
+    @Override
+    public void showLeftBtn(boolean show, int resourcesId) {
+        if (mActionBarBackBtn != null) {
+            if (show) {
+                mActionBarBackBtn.setImageResource(resourcesId);
+                mActionBarBackBtn.setVisibility(View.VISIBLE);
+            } else {
+                mActionBarBackBtn.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    @Override
+    public void showRightBtn(boolean show, int resourcesId) {
+        if (mActionBarRightBtn != null) {
+            if (show) {
+                mActionBarRightBtn.setImageResource(resourcesId);
+                mActionBarRightBtn.setVisibility(View.VISIBLE);
+            } else {
+                mActionBarRightBtn.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    @Override
+    public void showTitle(boolean show, String title) {
+        if (mActionBarTitle != null) {
+            if (show) {
+                mActionBarTitle.setText(title);
+                mActionBarTitle.setVisibility(View.VISIBLE);
+            } else {
+                mActionBarTitle.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    protected void showToast(String format, Object... objects) {
+        format = (format == null) ? "null" : ((objects != null && objects.length != 0) ?
+                String.format(Locale.US, format, objects) : format);
+        Toast.makeText(this, format, Toast.LENGTH_SHORT).show();
     }
 }
