8745000e4c6361a534cff5cf3e36ad0a9bec1555
huangxin <huang_xin88@163.com>
2021-04-24 22:25:17 +0800

    优化导航页

--- app/src/main/java/com/njxm/smart/activities/main/MainActivity.java
+++ app/src/main/java/com/njxm/smart/activities/main/MainActivity.java
@@ -8,13 +8,16 @@
 
 package com.njxm.smart.activities.main;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.Navigation;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
@@ -26,15 +29,45 @@ import com.ntxm.smart.R;
  */
 @Route(path = "/app/main")
 public class MainActivity extends AppBaseActivity<MainView, MainPresenter> implements MainView {
+    private static final int INVALID_ACTION = -1;
+    private final List<NavTab> navTabs = new ArrayList<>();
     private int lastSelected = R.id.first_btn;
-
-    private ConstraintLayout container;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.initViews();
-        this.container = this.findViewById(R.id.ll_root);
+        this.initNavTabs();
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private void initViews() {
+        RadioGroup radioGroup = this.findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            int actionId = this.getNavActionId(this.lastSelected, checkedId);
+            this.lastSelected = checkedId;
+            if (actionId != MainActivity.INVALID_ACTION) {
+                Navigation.findNavController(this, R.id.fragment_container).navigate(actionId);
+            }
+        });
+    }
+
+    /**
+     * 添加页面和导航action的映射关系
+     */
+    private void initNavTabs() {
+        this.navTabs.add(new NavTab(R.id.first_btn, R.id.btn2, R.id.action_attendanceFragment_to_workCenterFragment));
+        this.navTabs.add(new NavTab(R.id.first_btn, R.id.btn3, R.id.action_attendanceFragment_to_messageFragment));
+        this.navTabs.add(new NavTab(R.id.first_btn, R.id.btn4, R.id.action_attendanceFragment_to_personalFragment));
+        this.navTabs.add(new NavTab(R.id.btn2, R.id.first_btn, R.id.action_workCenterFragment_to_attendanceFragment));
+        this.navTabs.add(new NavTab(R.id.btn2, R.id.btn3, R.id.action_workCenterFragment_to_messageFragment));
+        this.navTabs.add(new NavTab(R.id.btn2, R.id.btn4, R.id.action_workCenterFragment_to_personalFragment));
+        this.navTabs.add(new NavTab(R.id.btn3, R.id.first_btn, R.id.action_messageFragment_to_attendanceFragment));
+        this.navTabs.add(new NavTab(R.id.btn3, R.id.btn2, R.id.action_messageFragment_to_workCenterFragment));
+        this.navTabs.add(new NavTab(R.id.btn3, R.id.btn4, R.id.action_messageFragment_to_personalFragment));
+        this.navTabs.add(new NavTab(R.id.btn4, R.id.first_btn, R.id.action_personalFragment_to_attendanceFragment));
+        this.navTabs.add(new NavTab(R.id.btn4, R.id.btn2, R.id.action_personalFragment_to_workCenterFragment));
+        this.navTabs.add(new NavTab(R.id.btn4, R.id.btn3, R.id.action_personalFragment_to_messageFragment));
     }
 
     @Override
@@ -46,67 +79,50 @@ public class MainActivity extends AppBaseActivity<MainView, MainPresenter> imple
     protected void onResume() {
         super.onResume();
         MainPresenter.fetchCountryAddresses();
-
     }
 
-    @SuppressLint("NonConstantResourceId")
-    private void initViews() {
-        RadioGroup radioGroup = this.findViewById(R.id.radio_group);
-        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
-            int selectedId = this.lastSelected;
-            if (this.lastSelected == checkedId) {
-                return;
+    private int getNavActionId(int from, int to) {
+        for (NavTab navTab : this.navTabs) {
+            if (navTab.equals(new NavTab(from, to, -1))) {
+                return navTab.action;
             }
-            this.lastSelected = checkedId;
-            int navId = -1;
-            switch (selectedId) {
-                case R.id.first_btn:
-                    if (checkedId == R.id.btn2) {
-                        navId = R.id.action_attendanceFragment_to_workCenterFragment;
-                    } else if (checkedId == R.id.btn3) {
-                        navId = R.id.action_attendanceFragment_to_messageFragment;
-                    } else if (checkedId == R.id.btn4) {
-                        navId = R.id.action_attendanceFragment_to_personalFragment;
-                    }
-                    break;
-                case R.id.btn2:
-                    if (checkedId == R.id.first_btn) {
-                        navId = R.id.action_workCenterFragment_to_attendanceFragment;
-                    } else if (checkedId == R.id.btn3) {
-                        navId = R.id.action_workCenterFragment_to_messageFragment;
-                    } else if (checkedId == R.id.btn4) {
-                        navId = R.id.action_workCenterFragment_to_personalFragment;
-                    }
-                    break;
-                case R.id.btn3:
-                    if (checkedId == R.id.first_btn) {
-                        navId = R.id.action_messageFragment_to_attendanceFragment;
-                    } else if (checkedId == R.id.btn2) {
-                        navId = R.id.action_messageFragment_to_workCenterFragment;
-                    } else if (checkedId == R.id.btn4) {
-                        navId = R.id.action_messageFragment_to_personalFragment;
-                    }
-                    break;
-                case R.id.btn4:
-                    if (checkedId == R.id.first_btn) {
-                        navId = R.id.action_personalFragment_to_attendanceFragment;
-                    } else if (checkedId == R.id.btn2) {
-                        navId = R.id.action_personalFragment_to_workCenterFragment;
-                    } else if (checkedId == R.id.btn3) {
-                        navId = R.id.action_personalFragment_to_messageFragment;
-                    }
-                    break;
-                default:
-            }
-            if (navId != -1) {
-                Navigation.findNavController(this, R.id.fragment_container).navigate(navId);
-            }
-        });
+        }
+        return MainActivity.INVALID_ACTION;
     }
 
     @NonNull
     @Override
     protected MainPresenter newPresenter() {
         return new MainPresenter();
+    }
+
+    private static class NavTab {
+        int from;
+        int to;
+        int action;
+
+        public NavTab(int from, int to, int action) {
+            this.from = from;
+            this.to = to;
+            this.action = action;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || this.getClass() != o.getClass()) {
+                return false;
+            }
+            NavTab navTab = (NavTab) o;
+            return this.from == navTab.from &&
+                    this.to == navTab.to;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.from, this.to);
+        }
     }
 }
